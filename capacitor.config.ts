64cde934ca1123536bd377c21ea15e69818c04ee
shanpalia <shanpalia786@gmail.com>
2026-08-17 11:25:20 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,19 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.shanpalia.paliaapkhub",
+  appName: "PaliaAPK HUB",
+  webDir: "www",
+  server: {
+    androidScheme: "https"
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ["google.com"],
+      googleWebClientId: "270953807883-btnln51tlh1e1b2dtjfo6bsoasjhoc3s.apps.googleusercontent.com"
+    }
+  }
+};
+
+export default config;
