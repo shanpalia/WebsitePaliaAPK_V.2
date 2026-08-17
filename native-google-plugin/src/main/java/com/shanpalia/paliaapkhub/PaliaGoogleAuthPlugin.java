713b267677cb3d2b2c5e8a67f732d0728240013a
shanpalia <shanpalia786@gmail.com>
2026-08-17 16:38:34 +0530

    Add files via upload

--- native-google-plugin/src/main/java/com/shanpalia/paliaapkhub/PaliaGoogleAuthPlugin.java
+++ native-google-plugin/src/main/java/com/shanpalia/paliaapkhub/PaliaGoogleAuthPlugin.java
@@ -7,7 +7,7 @@ import androidx.credentials.Credential;
 import androidx.credentials.CredentialManager;
 import androidx.credentials.CredentialManagerCallback;
 import androidx.credentials.CustomCredential;
-import androidx.credentials.GetCredentialException;
+import androidx.credentials.exceptions.GetCredentialException;
 import androidx.credentials.GetCredentialRequest;
 import androidx.credentials.GetCredentialResponse;
 
