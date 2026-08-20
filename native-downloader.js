d9af7f30576684405cdae5a3d0414bc35900fec6
shanpalia <shanpalia786@gmail.com>
2026-08-20 12:54:12 +0530

    Add files via upload

--- native-downloader.js
+++ native-downloader.js
@@ -1,44 +1,8 @@
-(function () {
-  let nativePlugin = null;
-
-  function isNativeAndroid() {
-    const C = window.Capacitor;
-    return !!(C && typeof C.isNativePlatform === "function" &&
-      C.isNativePlatform() && /android/i.test(navigator.userAgent || ""));
-  }
-
-  async function plugin() {
-    if (!isNativeAndroid()) return null;
-    const C = window.Capacitor;
-    if (C.Plugins && C.Plugins.PaliaDownloader) return C.Plugins.PaliaDownloader;
-    if (typeof C.registerPlugin === "function") {
-      nativePlugin = nativePlugin || C.registerPlugin("PaliaDownloader");
-      return nativePlugin;
-    }
-    return null;
-  }
-
-  window.paliaNativeAndroid = isNativeAndroid;
-
-  window.startPaliaApkDownload = async function (url, filename, expectedTotalBytes) {
-    const p = await plugin();
-    if (!p || typeof p.download !== "function") return null;
-    return await p.download({
-      url: String(url),
-      filename: filename || "PaliaAPK-HUB-App.apk",
-      expectedTotalBytes: Number(expectedTotalBytes || 0)
-    });
-  };
-
-  window.listenPaliaApkDownloadProgress = async function (callback) {
-    const p = await plugin();
-    if (!p || typeof p.addListener !== "function") return null;
-    return await p.addListener("downloadProgress", callback);
-  };
-
-  window.openPaliaApkInstaller = async function (filename) {
-    const p = await plugin();
-    if (!p || typeof p.openInstaller !== "function") return null;
-    return await p.openInstaller({ filename: String(filename) });
-  };
+(function(){
+function a(){const C=window.Capacitor;return !!(C&&C.isNativePlatform&&C.isNativePlatform()&&/android/i.test(navigator.userAgent||""))}
+let p=null;async function g(){const C=window.Capacitor;if(!a())return null;if(C.Plugins&&C.Plugins.PaliaDownloader)return C.Plugins.PaliaDownloader;if(C.registerPlugin){p=p||C.registerPlugin("PaliaDownloader");return p}return null}
+window.paliaNativeAndroid=a;
+window.startPaliaApkDownload=async(u,f,s)=>{const x=await g();return x?x.download({url:u,filename:f,expectedTotalBytes:s||0}):null};
+window.listenPaliaApkDownloadProgress=async cb=>{const x=await g();return x&&x.addListener?x.addListener("downloadProgress",cb):null};
+window.openPaliaApkInstaller=async f=>{const x=await g();return x?x.openInstaller({filename:f}):null};
 })();
